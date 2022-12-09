cdac8943ea05232a322aa10757f8f7428e6fe5a8
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2022-12-09 15:57:20 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import { TriuneBrain } from './triune';
 
 function App() {
+  const [brainState,setBrainState] = useState("neo")
+
   return (
-    <div className='w-3/5 mx-auto'>
-      <TriuneBrain />
+    <div className='w-3/5 mx-auto shadow-xl rounded-xl my-24 p-4'>
+        <p>Click on a Section of the brain to learn more</p>
+      <div className='flex'>
+        <div className='w-1/2'>
+          <TriuneBrain setState={setBrainState}/>
+        </div>
+        <div className='w-1/2'>
+          {brainState}
+        </div>
+      </div>
     </div>
   );
 }
